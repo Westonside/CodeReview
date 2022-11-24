0184e88ac58819cac8ec4737dd15db8f52002914
469512345 <e69512345@gmail.com>
2022-11-24 09:10:03 +0000

    Restructured project and fixing issues with the messaging service

--- frontend/front/src/components/HomeBody.js
+++ frontend/front/src/components/HomeBody.js
@@ -1,26 +1,26 @@
-import { Grid } from "@mui/material";
-import React, { useState } from "react";
-import { Paper } from "@mui/material";
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import { Box } from "@mui/system";
-import { Stack } from "@mui/material";
+import {Grid} from "@mui/material";
+import React, {useState} from "react";
+import {Paper} from "@mui/material";
+import {createTheme, ThemeProvider, styled} from '@mui/material/styles';
+import {Box} from "@mui/system";
+import {Stack} from "@mui/material";
 import Typography from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 // import WatchList from "./WatchList";
 // import BuyList from "./BuyList";
 // import TopStocks from "./TopStocks";
-const HomeBody = () =>{
-    const Item = styled(Paper)(({ theme }) => ({
+const HomeBody = () => {
+    const Item = styled(Paper)(({theme}) => ({
         ...theme.typography.body2,
         textAlign: 'center',
         color: theme.palette.text.secondary,
         minHeight: 100,
         lineHeight: '60px',
-        
-      }));
 
-    const startValues = [1,2,3]
+    }));
+
+    const startValues = [1, 2, 3]
     const [postContent, setPostContent] = useState("");
     const [postUser, setPostUser] = useState("");
 
@@ -29,55 +29,79 @@ const HomeBody = () =>{
     const [messageUserTwo, setMessageUserTwo] = useState("");
     const [contentMessage, setContentMessage] = useState("");
 
-    
 
-      const sendMessage = () =>{
+    const sendMessage = () => {
+        fetch("/api/messages/create/", {
+            method: "POST",
+            body: JSON.stringify({
+                from: messageUserOne,
+                to: messageUserTwo,
+                message: contentMessage
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(console.log);
+    }
 
-      }
+
+    const sendPost = () => {
+
+    }
 
 
-      const sendPost = () =>{
-        
-      }
+    return (
+        <Box sx={{flexGrow: 2, marginTop: 5, paddingLeft: 5, paddingRight: 5}}>
+            <TextField id="outlined-basic" label="Username" variant="outlined" value={postUser} onChange={(e) => {
+                setPostUser(e.target.value)
+            }}/>
+            <TextField id="outlined-basic" label="Post Content" variant="outlined" value={postContent}
+                       onChange={(e) => {
+                           setPostContent(e.target.value)
+                       }}/>
+            <Button variant="text" onClick={sendPost}>Submit</Button>
 
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <p>Top Stocks</p>
+                    {/* <TopStocks /> */}
+                </Grid>
+                <Grid item container spacing={4} sx={{marginTop: 5}}>
+                    <Grid item xs={7}>
+                        <Item>
 
-   return(
-    <Box sx={{ flexGrow: 2, marginTop:5, paddingLeft:5,paddingRight:5 }}>
-        <TextField id="outlined-basic" label="Username" variant="outlined" value={postUser}  onChange={(e)=>{setPostUser(e.target.value)}}/>
-        <TextField id="outlined-basic" label="Post Content" variant="outlined"  value={postContent} onChange={(e) =>{setPostContent(e.target.value)}}/>
-        <Button variant="text" onClick={sendPost}>Submit</Button>
-
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <p>Top Stocks</p>
-                {/* <TopStocks /> */}
-            </Grid>
-            <Grid item container spacing={4} sx={{marginTop:5}}>
-                <Grid item xs={7}>
-                    <Item>
-                        
-                        {startValues.map((val) =>{
-                            return(
-                                <Item>
-                                    <p>{val}</p>
-                                    <p>{`${val} user`}</p>
+                            {startValues.map((val) => {
+                                return (
+                                    <Item>
+                                        <p>{val}</p>
+                                        <p>{`${val} user`}</p>
                                     </Item>
-                            )
-                        })}
-                        {/* <WatchList /> */}
-                    </Item>
+                                )
+                            })}
+                            {/* <WatchList /> */}
+                        </Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <TextField id="outlined-basic" label="User sending message" variant="outlined"
+                                   value={messageUserOne} onChange={(e) => {
+                            setMessageUserOne(e.target.value)
+                        }}/>
+                        <TextField id="outlined-basic" label="User to send message" variant="outlined"
+                                   value={messageUserTwo} onChange={(e) => {
+                            setMessageUserTwo(e.target.value)
+                        }}/>
+                        <TextField id="outlined-basic" label="Message content" variant="outlined" value={contentMessage}
+                                   onChange={(e) => {
+                                       setContentMessage(e.target.value)
+                                   }} style={{marginTop: 5}}/>
+                        <Button variant="text" onClick={sendMessage}>Submit</Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                <TextField id="outlined-basic" label="User sending message" variant="outlined" value={messageUserOne} onChange={(e)=>{setMessageUserOne(e.target.value)}}/>
-                <TextField id="outlined-basic" label="User to send message" variant="outlined" value={messageUserTwo} onChange={(e)=>{setMessageUserTwo(e.target.value)}}/>
-                <TextField id="outlined-basic" label="Message content" variant="outlined" value={contentMessage} onChange={(e)=>{setContentMessage(e.target.value)}} style={{marginTop: 5 }}/>
-                <Button variant="text" onClick={sendMessage}>Submit</Button>
-                </Grid>
+
             </Grid>
-            
-        </Grid>
-        
-    {/* <Grid container spacing={2}>
+
+            {/* <Grid container spacing={2}>
       <Grid item xs={11} md={11} height={100} >
           <Item elevation={4}>
               <h2> testing</h2>
@@ -102,8 +126,8 @@ const HomeBody = () =>{
         <Item>xs=6 md=8</Item>
       </Grid>
     </Grid> */}
-  </Box>
-   )
+        </Box>
+    )
 }
 
 export default HomeBody;
--- messaging/src/main.rs
+++ messaging/src/main.rs
@@ -1,16 +1,16 @@
-#![feature(let_else)]
-
 use std::borrow::Borrow;
+use std::fmt::Debug;
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::string::ToString;
 
-use actix_web::{App, get, HttpResponse, HttpServer, middleware, post, web};
-use actix_web::http::header::ContentType;
+use actix_web::{App, get, HttpResponse, HttpServer, post, web};
+use actix_web::middleware::Logger;
 use mongodb::{Client, Collection, Database};
 use mongodb::bson::{DateTime, doc, Document};
 use mongodb::bson::oid::ObjectId;
 use mongodb::options::{ClientOptions, FindOptions};
 use futures::stream::{StreamExt};
+use serde::Serialize;
 
 const PORT: u16 = 22838;
 
@@ -24,7 +24,7 @@ struct MongoConnectionParams<'a> {
 
 impl MongoConnectionParams<'_> {
     fn to_uri(&self) -> String {
-        format!("mongodb://{}:{}@{}:{}/{}?w=majority", self.username, self.password, self.host, self.port, self.database)
+        format!("mongodb://{}:{}@{}:{}/{}?w=majority&authSource=admin", self.username, self.password, self.host, self.port, self.database)
     }
 }
 
@@ -52,13 +52,14 @@ async fn main() -> Result<(), CompositeError> {
         Ok(messages) => {
             let server_bind = HttpServer::new(move || {
                 App::new()
-                    // enable logger - always register actix-web Logger middleware last
-                    .wrap(middleware::Logger::default())
-                    // register HTTP requests handlers
+                    //Store the mongoDB connection handle as part of the webserver's data
                     .app_data(web::Data::new(AppData { messages: messages.clone() }))
+                    // register HTTP requests handlers
                     .service(list)
                     .service(list_from)
                     .service(create)
+                    // enable logger - always register actix-web Logger middleware last
+                    .wrap(Logger::default())
             })
                 .bind(SocketAddrV4::new(Ipv4Addr::LOCALHOST, PORT));
             match server_bind {
@@ -90,6 +91,7 @@ async fn connect_db() -> Result<Collection<Message>, mongodb::error::Error> {
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct Message {
+    #[serde(skip_serializing)]
     pub _id: Option<ObjectId>,
     pub time: DateTime,
     pub message: String,
@@ -128,7 +130,8 @@ impl MessageRequest {
 /// list 50 last tweets `/tweets`
 #[get("/api/messages/{user}")]
 async fn list(param: web::Path<(String, )>, data: web::Data<AppData>) -> HttpResponse {
-    let filter = doc! {"$or": [{"from": &param.0}, {"from": &param.0}]};
+    println!("/api/messages/user: {}", param.0);
+    let filter = doc! {"$or": [{"to": &param.0}, {"from": &param.0}]};
     let find_options: FindOptions = FindOptions::builder().sort(doc! {"time": -1}).build();
 
     send_response(filter, find_options, &data.messages).await
@@ -136,7 +139,8 @@ async fn list(param: web::Path<(String, )>, data: web::Data<AppData>) -> HttpRes
 
 #[get("/api/messages/{user1}/{user2}")]
 async fn list_from(param: web::Path<(String, String)>, data: web::Data<AppData>) -> HttpResponse {
-    let filter: Document = doc! {"$or": [{"from": &param.0, "to": &param.1}, {"from": &param.0, "to": &param.1}]};
+    println!("/api/messages/user1/user2: {}, {}", param.0, param.1);
+    let filter: Document = doc! {"$or": [{"from": &param.0, "to": &param.1}, {"from": &param.1, "to": &param.0}]};
     let find_options: FindOptions = FindOptions::builder().sort(doc! {"time": -1}).build();
 
     send_response(filter, find_options, &data.messages).await
@@ -144,35 +148,69 @@ async fn list_from(param: web::Path<(String, String)>, data: web::Data<AppData>)
 
 #[post("/api/messages/create/")]
 async fn create(request: web::Json<MessageRequest>, data: web::Data<AppData>) -> HttpResponse {
+    println!("/api/messages/create: {:?}", request);
     let Some(message) = request.to_message() else {
-        return HttpResponse::BadRequest().finish();
+        println!(" > unable to parse");
+        return HttpResponse::BadRequest().json(JsonResponse::failure("Bad request. Required fields: from, to, message"));
     };
+    println!(" > parsed: {:?}", message);
     let result = data.messages.insert_one(message, None).await;
     match result {
         Ok(insertion) => {
-            HttpResponse::Ok().content_type(ContentType::json()).json(insertion.inserted_id)
+            println!(" > inserted!");
+            HttpResponse::Ok().json(JsonResponse::success(insertion.inserted_id))
         }
-        Err(_) => {
-            HttpResponse::InternalServerError().finish()
+        Err(e) => {
+            println!(" > unable to insert: {:?}", e);
+            HttpResponse::InternalServerError().json(JsonResponse::failure("Unable to write to database"))
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct JsonResponse<T: Debug + Serialize> {
+    success: bool,
+    data: T,
+}
+
+impl<T: Debug + Serialize> JsonResponse<T> {
+    fn success(data: T) -> Self {
+        Self {
+            success: true,
+            data,
+        }
+    }
+}
+
+impl JsonResponse<String> {
+    fn failure(message: impl ToString) -> Self {
+        Self {
+            success: false,
+            data: message.to_string(),
         }
     }
 }
 
 async fn send_response(filter: Document, find_options: FindOptions, messages: &Collection<Message>) -> HttpResponse {
-    let Ok(mut cursor) = messages.find(filter, find_options).await else {
-        return HttpResponse::InternalServerError().finish();
-    };
-    let mut messages: Vec<Message> = vec!();
+    match messages.find(filter, find_options).await {
+        Ok(mut cursor) => {
+            println!(" > found");
+            let mut messages: Vec<Message> = vec!();
 
-    while let Some(message) = cursor.next().await {
-        match message {
-            Ok(e) => {
-                messages.push(e)
+            while let Some(message) = cursor.next().await {
+                match message {
+                    Ok(e) => {
+                        messages.push(e)
+                    }
+                    Err(_) => {}
+                }
             }
-            Err(_) => {}
+            HttpResponse::Ok()
+                .json(JsonResponse::success(messages))
+        }
+        Err(e) => {
+            println!(" > unable to find: {:?}", e);
+            return HttpResponse::InternalServerError().json(JsonResponse::failure("Unable to fetch from the database"));
         }
     }
-    HttpResponse::Ok()
-        .content_type(ContentType::json())
-        .json(messages)
 }
